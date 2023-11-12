ad8fff5186556bb78a343c5079ee20c64ae4e66e
Angela Ku <anyaku@ucsc.edu>
2023-11-12 12:31:18 -0800

    Add custom font through CSS

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@
 /* Time:   ?
 /*
 /* Citations
-/* - 
+/* - External CSS           - https://www.w3schools.com/css/css_howto.asp
+/* - Phaser 3 Custom Fonts  - https://learn.yorkcs.com/2019/09/28/phaser-3-basics-custom-fonts/
+/* - Pad Zeroes             - https://stackoverflow.com/questions/10073699/pad-a-number-with-leading-zeros-in-javascript
 /**********************************************************************************************/
 
 let config = {
@@ -30,5 +32,7 @@ let cursors, keys, escKey
 let game = new Phaser.Game(config)
 
 let { height, width } = game.config // Destructuring an object and assigning its properties to variables
+
+let score = 0
 
 let musicPlaying = false
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,10 +8,25 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
+        let playerTextConfig = {
+            fontFamily: 'Thou Art Dead',
+            fontSize: '26px',
+            color: '#670002',
+            align: 'right'
+        }
 
+        let scoreTextConfig = {
+            fontFamily: 'Thou Art Dead',
+            fontSize: '29px',
+            color: '#670002',
+            align: 'right'
+        }
+
+        this.playerText = this.add.text(44, height - 55, 'PLAYER 1', playerTextConfig);
+        this.scoreText = this.add.text(width - 210, 20, '', scoreTextConfig);
     }
 
     update() {
-
+        this.scoreText.text = String(Phaser.Math.Clamp(score, 0, 99999)).padStart(5, '0')
     }
 }
